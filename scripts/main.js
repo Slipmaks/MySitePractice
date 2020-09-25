8180696aaf72bf1d775b533f8ff36269fc70bb12
Slipmaks <slipmaks@gmail.com>
2020-09-25 15:46:26 +0300

    media bug

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,8 @@
 var siteHeader = document.querySelector('.h1-header');
 siteHeader.textContent = "Lyrics list of Meshuggah songs"
+
+/*var lyricBox = document.querySelector('.lyric');
+const fileUrl = 'media/text.txt'
+fetch(fileUrl)
+    .then(r => r.text)
+    .then(t => console.log(t))*/
